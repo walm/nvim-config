c65ce14d2cdcf66960dd97f17ce47c1296f0d59c
Andreas Wålm <andreas@walm.net>
2026-04-12 13:47:30 +0200

    force filetype typescriptreact for ts files

--- lua/walm/core/autocmd.lua
+++ lua/walm/core/autocmd.lua
@@ -7,3 +7,10 @@ vim.api.nvim_create_autocmd("FileType", {
     -- vim.opt_local.spell = true
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  pattern = "*.ts",
+  callback = function()
+    vim.bo.filetype = "typescriptreact"
+  end,
+})
