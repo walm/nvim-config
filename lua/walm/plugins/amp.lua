7a168d6cf07631c1c52f42f92047dcf55f195fdf
Andreas Wålm <andreas@walm.net>
2026-05-13 05:42:58 +0200

    disable amp plugin

--- lua/walm/plugins/amp.lua
+++ lua/walm/plugins/amp.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    "sourcegraph/amp.nvim",
-    branch = "main",
-    lazy = false,
-    opts = { auto_start = true, log_level = "info" },
-  },
+  -- {
+  --   "sourcegraph/amp.nvim",
+  --   branch = "main",
+  --   lazy = false,
+  --   opts = { auto_start = true, log_level = "info" },
+  -- },
 }
