3c8ed80888c6a4c84e376af06b1485efb1a6b430
Andreas Wålm <andreas@walm.net>
2026-06-05 07:26:14 +0200

    Add <leader>gb to blame current line via gitsigns

--- lua/walm/plugins/gitsigns.lua
+++ lua/walm/plugins/gitsigns.lua
@@ -33,6 +33,7 @@ return {
 			map("n", "<leader>hb", function()
 				gs.blame_line({ full = true })
 			end, "Blame line")
+			map("n", "<leader>gb", gs.blame_line, "Blame line")
 			map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
 
 			map("n", "<leader>hd", gs.diffthis, "Diff this")
