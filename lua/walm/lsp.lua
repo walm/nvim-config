5f4f3d6a392ba648a93d4c5a7275adb5984c06ca
Andreas Wålm <andreas@walm.net>
2026-07-08 17:40:33 +0200

    Use Snacks picker instead of Telescope for LSP mappings

--- lua/walm/lsp.lua
+++ lua/walm/lsp.lua
@@ -8,19 +8,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     -- set keybinds
     opts.desc = "Show LSP references"
-    keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+    keymap.set("n", "gR", function()
+      Snacks.picker.lsp_references()
+    end, opts) -- show definition, references
 
     opts.desc = "Go to declaration"
     keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
     opts.desc = "Show LSP definition"
-    keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definition
+    keymap.set("n", "gd", function()
+      Snacks.picker.lsp_definitions()
+    end, opts) -- show lsp definition
 
     opts.desc = "Show LSP implementations"
-    keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+    keymap.set("n", "gi", function()
+      Snacks.picker.lsp_implementations()
+    end, opts) -- show lsp implementations
 
     opts.desc = "Show LSP type definitions"
-    keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+    keymap.set("n", "gt", function()
+      Snacks.picker.lsp_type_definitions()
+    end, opts) -- show lsp type definitions
 
     opts.desc = "See available code actions"
     keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -29,7 +37,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
     keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
     opts.desc = "Show buffer diagnostics"
-    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+    keymap.set("n", "<leader>D", function()
+      Snacks.picker.diagnostics_buffer()
+    end, opts) -- show  diagnostics for file
 
     opts.desc = "Show line diagnostics"
     keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
